427a5e01e4a4b309305ffdfc034cb4cd4d2273ac
SebaSphere <SebaSphereEmails@gmail.com>
2021-04-23 16:01:07 -0400

    FEAT: Sort of found the method for block break cancelling. Need to find a better hook tho

--- src/main/java/dev/sebastianb/ffactions/FFactions.java
+++ src/main/java/dev/sebastianb/ffactions/FFactions.java
@@ -1,10 +1,14 @@
 package dev.sebastianb.ffactions;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.mixin.event.lifecycle.ServerWorldMixin;
+import net.minecraft.server.command.SetWorldSpawnCommand;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class FFactions implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        System.out.println("loaded mod factions");
     }
 }
