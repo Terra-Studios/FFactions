06e7a922a727f5a3a64acce1a2f1b5f42e4fdfee
SebaSphere <SebaSphereEmails@gmail.com>
2021-07-20 07:39:30 -0400

    CHORE: Add more stuff to enum

--- src/main/java/dev/sebastianb/ffactions/permission/FactionMemberRanks.java
+++ src/main/java/dev/sebastianb/ffactions/permission/FactionMemberRanks.java
@@ -1,18 +1,45 @@
 package dev.sebastianb.ffactions.permission;
 
 public enum FactionMemberRanks {
-    LEADER(null),
-    OFFICER(null),
-    MEMBER(OFFICER),
-    VISITOR(MEMBER);
+    LEADER(5), // skipped a number because I don't want someone to be promoted to "leader". Always keep a gap for new enum values
+    OFFICER(3),
+    MEMBER(2),
+    VISITOR(1);
 
-    private final FactionMemberRanks rankParent;
+    private final int rankWeight;
 
-    FactionMemberRanks(FactionMemberRanks ranks) {
-        this.rankParent = ranks;
+    FactionMemberRanks(int weight) {
+        this.rankWeight = weight;
     }
 
-    public FactionMemberRanks getRankParent() {
-        return rankParent;
+    public FactionMemberRanks getParentRank(FactionMemberRanks rank) {
+        return getRelativeRank(rank, 1);
+    }
+
+    public FactionMemberRanks getChildRank(FactionMemberRanks rank) {
+        return getRelativeRank(rank, -1);
+    }
+
+    private FactionMemberRanks getRelativeRank(FactionMemberRanks rank, int relRankNum) {
+        FactionMemberRanks relRank = null;
+        int i = 0;
+        try {
+            for (FactionMemberRanks currentRank : FactionMemberRanks.values()) {
+                if (currentRank.getRankWeight() + relRankNum == rank.getRankWeight()) {
+                    relRank = currentRank;
+                    i++;
+                }
+                if (i > 1) {
+                    throw new Exception("Calling multiple parents and/or children for faction members. This should not happen please report.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return relRank;
+    }
+
+    private int getRankWeight() {
+        return rankWeight;
     }
 }
